c015e71f70bc4a2a31b3fdd42c8370d0ab24c66f
UltraNote <support@ultranote.org>
2020-05-31 18:28:37 +0200

    update key

--- js/crypto_utils.js
+++ js/crypto_utils.js
@@ -1625,9 +1625,9 @@ return{_strlen:lb,_ge_mul8:Va,_keccak:db,_ge_scalarmult:Ta,_ge_fromfe_frombytes_
 var salt = '4721283fee2db41e1f2fc50ea9f6d783'; // IMPORTANT: never change it, otherwise it will be impossible to restore previously generated wallets!
 var config = {
     coinUnitPlaces: 6,
-    coinSymbol: 'CCX',
-    coinName: 'Conceal',
-    coinUriPrefix: 'conceal:',
+    coinSymbol: 'XUNI',
+    coinName: 'UltraNote Infinity',
+    coinUriPrefix: 'ultranotei:',
     addressPrefix: 31444
 };
 var cnUtil = (function(initConfig) {
